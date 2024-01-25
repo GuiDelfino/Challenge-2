ab9760e139c2ac7b054d2878db060bb80f62c1e9
Rafael Rodrigues <rafael-rodrigues-@live.com>
2024-01-24 21:11:43 -0300

    Initial Commit

---
+++ Challenge-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge-2
+//
+//  Created by Rafael Rodrigues on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
